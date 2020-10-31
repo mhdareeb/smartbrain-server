9da4555fd8d6f4b34916316a5c3cac4f2618840c
Mohammad Areeb <areeb.mohammad11@gmail.com>
2020-10-31 17:57:45 +0530

    added duplicate registration check

--- routes/register.js
+++ routes/register.js
@@ -3,25 +3,33 @@ const handleRegister = (req, res, db, bcrypt) => {
     if(!(name.length>0 && email.includes('@') && email.includes('.com') && password.length>=3))
         return res.json("invalid");
     const hash = bcrypt.hashSync(password);
-    return db.transaction(trx => {
-        trx('login').insert({
-            email: email,
-            hash : hash
-        })
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users').insert({
-                name:name,
-                email:loginEmail[0],
+    db.select('*').from('login').where('email','=',email)
+    .then(info=>{
+        if(info.length>0)
+            return res.json('exists');
+        else
+        {
+            return db.transaction(trx => {
+                trx('login').insert({
+                    email: email,
+                    hash : hash
+                })
+                .returning('email')
+                .then(loginEmail => {
+                    return trx('users').insert({
+                        name:name,
+                        email:loginEmail[0],
+                    })
+                    .returning('*')
+                    .then(user=>res.json(user[0]));
+                })
+                .then(trx.commit)
+                .catch(err=>{
+                    trx.rollback;
+                    res.status(400).json('failed');
+                });
             })
-            .returning('*')
-            .then(user=>res.json(user[0]));
-        })
-        .then(trx.commit)
-        .catch(err=>{
-            trx.rollback;
-            res.status(400).json('failed');
-        });
+        }
     })
     .catch(err=>res.status(400).json('failed'));
 }
